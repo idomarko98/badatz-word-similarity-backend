d16eb67d244bad8f10bc1251ee7589e81326194f
Ido <Ido>
2024-03-01 21:50:25 +0200

    add test

---
+++ src/test/kotlin/com/example/words/similarities/WordsServiceTest.kt
@@ -0,0 +1,33 @@
+package com.example.words.similarities
+
+import com.example.words.similarities.entities.Word
+import com.example.words.similarities.repos.WordsRepository
+import com.example.words.similarities.services.WordsService
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WordsServiceTest() {
+
+    private final val mockedWordsRepository = mockk<WordsRepository>()
+
+    val wordService = WordsService(mockedWordsRepository)
+
+    @Test
+    fun `Given a word, When trying to find similar words, return similar words`() {
+        val word = "apple"
+        val expectedKeyWord = "aelpp"
+        val firstExpectedWord = Word(expectedKeyWord, "apple")
+        val secondExpectedWord = Word(expectedKeyWord, "pepla")
+        val expectedWordList = listOf(firstExpectedWord, secondExpectedWord)
+
+        every { mockedWordsRepository.findAllByKeyWord(expectedKeyWord) } returns expectedWordList
+
+        val actualList = wordService.getSimilarWords(word)
+
+        assert(actualList == expectedWordList)
+
+    }
+}
