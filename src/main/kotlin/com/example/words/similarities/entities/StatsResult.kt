1cadbc34113ea16ab6bd62a5acb0cd090f9349b5
Ido <Ido>
2024-03-02 16:41:51 +0200

    add tests for stats result

--- src/main/kotlin/com/example/words/similarities/entities/StatsResult.kt
+++ src/main/kotlin/com/example/words/similarities/entities/StatsResult.kt
@@ -1,3 +1,23 @@
 package com.example.words.similarities.entities
 
-class StatsResult(val totalWords: Int, val totalRequest: Int, val avgProcessingTime: Int?)
+class StatsResult(val totalWords: Int, val totalRequest: Int, val avgProcessingTime: Int?) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as StatsResult
+
+        if (totalWords != other.totalWords) return false
+        if (totalRequest != other.totalRequest) return false
+        if (avgProcessingTime != other.avgProcessingTime) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = totalWords
+        result = 31 * result + totalRequest
+        result = 31 * result + (avgProcessingTime ?: 0)
+        return result
+    }
+}
---
+++ src/test/kotlin/com/example/words/similarities/StatsServiceTest.kt
@@ -0,0 +1,62 @@
+package com.example.words.similarities
+
+import com.example.words.similarities.entities.SimilarResult
+import com.example.words.similarities.entities.StatsResult
+import com.example.words.similarities.entities.Word
+import com.example.words.similarities.exception.BadRequestException
+import com.example.words.similarities.exception.DuplicateKeyException
+import com.example.words.similarities.repos.ApiRequestsRepository
+import com.example.words.similarities.repos.WordsRepository
+import com.example.words.similarities.services.StatsService
+import com.example.words.similarities.services.WordsService
+import io.mockk.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.springframework.boot.test.context.SpringBootTest
+import java.time.Instant
+
+@SpringBootTest
+class StatsServiceTest {
+
+    private final val mockedWordsRepository = mockk<WordsRepository>()
+    private final val mockedApiRequestRepository = mockk<ApiRequestsRepository>()
+
+    val statsService: StatsService = StatsService(mockedWordsRepository, mockedApiRequestRepository)
+
+    @Test
+    fun `Given no timeframe, When getting stats, Then return stats`() {
+        val fromDate = null
+        val toDate = null
+        val expectedTotalWords = 1L
+        val expectedTotalReqs = 2L
+        val expectedAvgTime = 182.0
+        every { mockedWordsRepository.count() } returns expectedTotalWords
+        every { mockedApiRequestRepository.count() } returns expectedTotalReqs
+        every { mockedApiRequestRepository.getAverageProcessingTime() } returns expectedAvgTime
+        val expectedStatResult =
+            StatsResult(expectedTotalWords.toInt(), expectedTotalReqs.toInt(), expectedAvgTime.toInt())
+
+        val actualStatResult = statsService.getStats(fromDate, toDate)
+
+        assert(expectedStatResult == actualStatResult)
+    }
+
+    @Test
+    fun `Given timeframe, When getting stats, Then return stats`() {
+        val fromDate = Instant.parse("2022-01-01T00:00:00Z")
+        val toDate = Instant.parse("2024-01-01T00:00:00Z")
+        val expectedTotalWords = 1
+        val expectedTotalReqs = 2
+        val expectedAvgTime = 182.0
+        every { mockedWordsRepository.countWithTimeFrame(fromDate,toDate) } returns expectedTotalWords
+        every { mockedApiRequestRepository.countWithTimeFrame(fromDate, toDate) } returns expectedTotalReqs
+        every { mockedApiRequestRepository.getAverageProcessingTimeWithTimeFrame(fromDate,toDate) } returns expectedAvgTime
+        val expectedStatResult =
+            StatsResult(expectedTotalWords, expectedTotalReqs, expectedAvgTime.toInt())
+
+        val actualStatResult = statsService.getStats(fromDate, toDate)
+
+        assert(expectedStatResult == actualStatResult)
+    }
+
+}
