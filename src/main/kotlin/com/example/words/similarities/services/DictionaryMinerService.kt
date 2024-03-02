261dfea422f4de35c5c8c56e4cf1ca5515fdc3a0
Ido <Ido>
2024-03-02 17:03:42 +0200

    delete redundant timestamp

--- src/main/kotlin/com/example/words/similarities/services/DictionaryMinerService.kt
+++ src/main/kotlin/com/example/words/similarities/services/DictionaryMinerService.kt
@@ -3,18 +3,15 @@ package com.example.words.similarities.services
 import org.springframework.beans.factory.annotation.Autowired
 import java.io.BufferedReader
 import java.io.FileReader
-import java.time.Instant
 
 class DictionaryMinerService(@Autowired val wordsService: WordsService) {
 
     fun mineDictionary(path: String) {
-        val timestamp = Instant.now()
-
         BufferedReader(FileReader(path)).use { reader ->
             var line: String?
 
             while (reader.readLine().also { line = it } != null) {
-                if(line != ""){
+                if (line != "") {
                     wordsService.addNewWord(line!!)
                 }
             }
