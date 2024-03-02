f7c4218998e19b32e900e5949963c0500764e68b
Ido <Ido>
2024-03-02 18:32:11 +0200

    remove redundant imports

--- src/test/kotlin/com/example/words/similarities/StatsServiceTest.kt
+++ src/test/kotlin/com/example/words/similarities/StatsServiceTest.kt
@@ -1,17 +1,11 @@
 package com.example.words.similarities
 
-import com.example.words.similarities.entities.SimilarResult
 import com.example.words.similarities.entities.StatsResult
-import com.example.words.similarities.entities.Word
-import com.example.words.similarities.exception.BadRequestException
-import com.example.words.similarities.exception.DuplicateKeyException
 import com.example.words.similarities.repos.ApiRequestsRepository
 import com.example.words.similarities.repos.WordsRepository
 import com.example.words.similarities.services.StatsService
-import com.example.words.similarities.services.WordsService
 import io.mockk.*
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.assertThrows
 import org.springframework.boot.test.context.SpringBootTest
 import java.time.Instant
 
