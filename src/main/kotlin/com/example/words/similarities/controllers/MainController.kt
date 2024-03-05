814591700bd5ba5f4b23bc41d48cd15d6d4a253a
Ido <Ido>
2024-03-05 22:23:08 +0200

    add crossorigin

--- src/main/kotlin/com/example/words/similarities/controllers/MainController.kt
+++ src/main/kotlin/com/example/words/similarities/controllers/MainController.kt
@@ -13,16 +13,19 @@ import java.time.Instant
 class MainController(@Autowired val wordsService: WordsService, @Autowired val statsService: StatsService) {
 
     @GetMapping("/api/v1/similar")
+    @CrossOrigin(origins = ["http://localhost:3000"])
     fun getSimilarity(@RequestParam word: String): SimilarResult {
         return wordsService.getSimilarWords(word)
     }
 
     @GetMapping("/api/v1/stats")
-    fun getStats(@RequestParam from: Instant?, @RequestParam to: Instant?): StatsResult {
+    @CrossOrigin(origins = ["http://localhost:3000"])
+    fun getStats(@RequestParam(name = "from") from: Instant?, @RequestParam(name = "to") to: Instant?): StatsResult {
         return statsService.getStats(from, to)
     }
 
     @PostMapping("/api/v1/add-word")
+    @CrossOrigin(origins = ["http://localhost:3000"])
     fun addNewWord(@RequestBody wordToAdd: WordToAdd) {
         return wordsService.addNewWord(wordToAdd.word)
     }
